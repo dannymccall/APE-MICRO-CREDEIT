2c715472c1a6c28e050ee9bf9e8327ff7ec25918
dannymccall <dannymccall419@gmail.com>
2025-02-18 16:53:25 +0000

    fixing activities/route

--- app/api/activities/route.ts
+++ app/api/activities/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
  import { Activitymanagement } from "@/app/lib/backend/models/activitymanagement.model";
  import { revalidatePath } from "next/cache";
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest) {
   try {
     const searchParams = req.nextUrl.searchParams;
 
