ebb9b58ad9127e5be8f1a144df9d6cc7fb33c034
dannymccall <dannymccall419@gmail.com>
2025-07-17 15:18:38 +0000

    error

--- app/component/loans/LoanPaymentApprovals/LoanApproval.tsx
+++ app/component/loans/LoanPaymentApprovals/LoanApproval.tsx
@@ -74,7 +74,7 @@ const LoanApproval: React.FC<LoanApprovalProps> = ({ pendingLoan, onApprove }) =
           {pendingLoan.client.first_name} {pendingLoan.client.last_name}
         </td>
         <td className="p-2 text-sm">
-          {pendingLoan && pendingLoan.loan.nextPayment ? formatDate(pendingLoan.loan.nextPayment):"N/A"}
+          {pendingLoan && pendingLoan.loan && pendingLoan.loan.nextPayment ? formatDate(pendingLoan.loan.nextPayment):"N/A"}
         </td>
         <td className="p-2">
           GHS {Number(pendingLoan.weeklyAmountExpected).toFixed(2)}
